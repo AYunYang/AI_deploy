ae75d0e724941caf502b3f0262cd06256d411d7f
AYunYang <angyunyanghyy@gmail.com>
2025-01-16 10:19:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import uvicorn
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("routes:app", host="0.0.0.0", port=8000, reload=True, workers=1) #, limit_concurrency=1, limit_max_requests=1)
+    uvicorn.run("routes:app", host="0.0.0.0", port=8080, reload=True, workers=1) #, limit_concurrency=1, limit_max_requests=1)
