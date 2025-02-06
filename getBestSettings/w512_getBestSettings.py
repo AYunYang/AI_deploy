421a07b36fd1cdc77a8ea12d8b48fd5e6dddcef3
AYunYang <yunyang.22@ichat.sp.edu.sg>
2025-02-07 00:03:38 +0800

    Normalized the time and energy

--- getBestSettings/w512_getBestSettings.py
+++ getBestSettings/w512_getBestSettings.py
@@ -11,6 +11,8 @@ import time as clock
 
 
 def execute_w512_getBestSettings(temp):
+    weight_energy=0.5
+    weight_time=0.5
     aircon_status_result = pd.read_csv("saved_data/aircon_status_W512_getBestSettings.csv")
 
     No_of_aircon = len([
@@ -18,7 +20,15 @@ def execute_w512_getBestSettings(temp):
         if "FC_Unit_" in col and "_Status" in col and "Fan" not in col
     ])
 
-           
+    lowest_timetaken = aircon_status_result["total_time_maintained"].min()
+    highest_timetaken = aircon_status_result["total_time_maintained"].max()
+    lowest_energy = aircon_status_result["total_energy_consumption"].min()
+    highest_energy = aircon_status_result["total_energy_consumption"].max()
+
+    aircon_status_result["total_time_maintained"] = (aircon_status_result["total_time_maintained"] - lowest_timetaken)/(highest_timetaken - lowest_timetaken)
+    aircon_status_result["total_energy_consumption"] = (aircon_status_result["total_energy_consumption"] - lowest_energy)/(highest_energy - lowest_energy)
+
+    aircon_status_result.to_csv("del ltrrrrrrr.csv")
     def getClosestTempToMaintain(maintain_temp):
         closest_temp_index = None
         smallest_difference = float('inf')
@@ -55,9 +65,13 @@ def execute_w512_getBestSettings(temp):
 
         filtered_data = aircon_status_result[aircon_status_result["current_temp"] == closest_temp]
 
-        # Sort by most time maintained (descending) and least energy consumption (ascending)
+        # Sort by most time maintained (descending) and least energy consumption (ascending) not using enegy efficiency for now
+        # filtered_data["efficiency_score"] = (filtered_data["total_energy_consumption"] * weight_energy) - (filtered_data["total_time_maintained"] * weight_time)
+        # best_entries = filtered_data.loc[filtered_data["efficiency_score"].idxmin()]
+
+
         best_entries = filtered_data.sort_values(
-            by=["total_energy_consumption","total_time_maintained"], ascending=[True,False]
+            by=[ "total_energy_consumption","total_time_maintained"], ascending=[True,False]
         ).iloc[0]
 
         formatted_results = [
@@ -65,7 +79,7 @@ def execute_w512_getBestSettings(temp):
                 "others": {
                     "current_temperature": float(best_entries["current_temp"]),
                     "current_humidity": float(best_entries["current_humi"]), 
-                    "total_time_maintained": int(best_entries["total_time_maintained"]), 
+                    "total_time_maintained": float(best_entries["total_time_maintained"]), 
                     "total_energy_consumption": float(best_entries["total_energy_consumption"]),  
                     "energy_efficiency": float(best_entries["energy_efficiency"])
                 }
